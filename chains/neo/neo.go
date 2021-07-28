1a96c698a5f28c9cce07d8c7cca0a73f1d102493
Stefan Liu <stefanliu@outlook.com>
2021-07-28 17:35:09 +0800

    Add neo sdk

---
+++ chains/neo/neo.go
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2021 The poly network Authors
+ * This file is part of The poly network library.
+ *
+ * The  poly network  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The  poly network  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The poly network .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neo
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/joeqian10/neo-gogogo/rpc"
+	"github.com/polynetwork/bridge-common/chains"
+)
+
+type Rpc = rpc.RpcClient
+
+type Client struct {
+	*Rpc
+	address string
+}
+
+func New(url string) *Client {
+	client := rpc.NewClient(url)
+	return &Client{
+		Rpc:     client,
+		address: url,
+	}
+}
+
+func (c *Client) Address() string {
+	return c.address
+}
+
+func (c *Client) GetLatestHeight() (uint64, error) {
+	res := c.GetBlockCount()
+	if res.ErrorResponse.Error.Message != "" {
+		return 0, fmt.Errorf("%s", res.ErrorResponse.Error.Message)
+	}
+	return uint64(res.Result), nil
+}
+
+type SDK struct {
+	*chains.ChainSDK
+	nodes []*Client
+}
+
+func (s *SDK) Node() *Client {
+	return s.nodes[s.ChainSDK.Index()]
+}
+
+func (s *SDK) Select() *Client {
+	return s.nodes[s.ChainSDK.Select()]
+}
+
+func NewSDK(chainID uint64, urls []string, interval time.Duration, maxGap uint64) (*SDK, error) {
+
+	clients := make([]*Client, len(urls))
+	nodes := make([]chains.SDK, len(urls))
+	for i, url := range urls {
+		client := New(url)
+		nodes[i] = client
+		clients[i] = client
+	}
+	sdk, err := chains.NewChainSDK(chainID, nodes, interval, maxGap)
+	if err != nil {
+		return nil, err
+	}
+	return &SDK{ChainSDK: sdk, nodes: clients}, nil
+}
