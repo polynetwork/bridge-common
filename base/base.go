fd90719f513c5d24f2dc678c5d2da56b17748d3b
Stefan Liu <stefanliu@outlook.com>
2021-09-09 20:41:55 +0800

    Correct blocks to wait for chains

--- base/base.go
+++ base/base.go
@@ -99,14 +99,14 @@ func BlocksToWait(chainId uint64) uint64 {
 	switch chainId {
 	case ETH:
 		return 12
-	case BSC:
-		return 3
-	case HECO:
-		return 3
-	case ONT:
+	case BSC, HECO:
+		return 21
+	case ONT, NEO, NEO3, OK, SWITCHEO:
 		return 1
-	case NEO:
-		return 1
+	case O3:
+		return 12
+	case MATIC:
+		return 128
 	default:
 		return 100000000
 	}
