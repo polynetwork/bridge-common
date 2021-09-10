81dfb21737de2375653a94d4396e1f37d9135bc5
Stefan Liu <stefanliu@outlook.com>
2021-09-10 21:35:12 +0800

    Add ont wallet support

---
+++ wallet/ont.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021 The poly network Authors
+ * This file is part of The poly network library.
+ *
+ * The  poly network  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The  poly network  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The poly network .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package wallet
+
+import (
+	"fmt"
+
+	sdk "github.com/ontio/ontology-go-sdk"
+)
+
+func NewOntSigner(config *Config) (signer *sdk.Account, err error) {
+	if config == nil {
+		return nil, fmt.Errorf("Missing ont wallet config")
+	}
+	s := sdk.NewOntologySdk()
+	wallet, err := s.OpenWallet(config.Path)
+	if err != nil {
+		err = fmt.Errorf("Open ont wallet error %v", err)
+		return nil, err
+	}
+	signer, err = wallet.GetDefaultAccount([]byte(config.Password))
+	if err != nil || signer == nil {
+		signer, err = wallet.NewDefaultSettingAccount([]byte(config.Password))
+		if err != nil {
+			err = fmt.Errorf("Get ont default account error %v", err)
+			return
+		}
+		err = wallet.Save()
+	}
+	return
+}
