110230b25b50d103b32b311f5a091ec990af71bf
Stefan Liu <stefanliu@outlook.com>
2021-11-03 18:13:26 +0800

    Change chain id for test eth and bsc

--- base/testnet.go
+++ base/testnet.go
@@ -22,11 +22,11 @@ package base
 var (
 	POLY     uint64 = 0
 	BTC      uint64 = 1
-	ETH      uint64 = 2
+	ETH      uint64 = 1002
 	ONT      uint64 = 3
 	NEO      uint64 = 5
 	HECO     uint64 = 7
-	BSC      uint64 = 79
+	BSC      uint64 = 6
 	O3       uint64 = 82
 	NEO3     uint64 = 88
 	PLT      uint64 = 107
