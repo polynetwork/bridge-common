af4e70a900b8b8bb98a06792f18ea1ad89c3b48b
Stefan Liu <stefanliu@outlook.com>
2021-11-15 17:57:21 +0800

    Change chain id again

--- base/devnet.go
+++ base/devnet.go
@@ -22,10 +22,10 @@ package base
 var (
 	POLY     uint64 = 0
 	BTC      uint64 = 1
-	ETH      uint64 = 1000002
+	ETH      uint64 = 202
 	ONT      uint64 = 3
 	NEO      uint64 = 5
-	HECO     uint64 = 100007
+	HECO     uint64 = 207
 	BSC      uint64 = 1000006
 	O3       uint64 = 82
 	NEO3     uint64 = 88
