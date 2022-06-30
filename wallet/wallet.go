f4ffa0895afdd3659fd5fae8838cd0c259d3efc4
rain-zxn <206131521@qq.com>
2022-06-30 17:51:06 +0800

    upgrade gaslimit to *1.1

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -36,6 +36,7 @@ import (
 type Config struct {
 	ChainId           uint64
 	KeyStoreProviders []*KeyStoreProviderConfig
+	KeyProviders      []string
 	Nodes             []string
 
 	// NEO wallet
@@ -324,6 +325,7 @@ func (w *Wallet) SendWithMaxLimit(account accounts.Account, addr common.Address,
 		return
 	}
 
+	limit = uint64(1.1 * float32(limit))
 	if maxLimit.Cmp(new(big.Int).Mul(big.NewInt(int64(limit)), gasPrice)) == -1 {
 		nonces.Update(false)
 		err = fmt.Errorf("Send tx estimated gas (limit %v, price %s) higher than max limit %s", limit, gasPrice, maxLimit)
