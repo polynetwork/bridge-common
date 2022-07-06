a8dd9edc7f50c5ec59b1c1eb595879a70e2521fc
Stefan Liu <stefanliu@outlook.com>
2022-07-06 10:28:34 +0800

    Upgrade zion-sdk

--- chains/eth/eth_test.go
+++ chains/eth/eth_test.go
@@ -6,17 +6,18 @@ import (
 )
 
 func TestClient_HeaderByNumber(t *testing.T) {
-	c := New("https://bsc-dataseed1.ninicoin.io")
+	c := New("https://ethereum-mainnet-rpc.allthatnode.com/")
 	height, err := c.GetLatestHeight()
 	if err != nil {
 		t.Fatal("Failed to get latest height", err)
 	}
 	t.Log("Chain height", height)
-	header, err := c.GetHeader(19123778)
+	header, err := c.GetHeader(15_050_000)
 	if err != nil {
 		t.Fatal("Failed to get header by number ", err)
 	}
 	t.Log(fmt.Sprintf("%s", header))
+	t.Log(fmt.Sprintf("%x", header))
 	h, err := header.GetHeight()
 	if err != nil { t.Fatal(err) }
 	t.Log(h)
